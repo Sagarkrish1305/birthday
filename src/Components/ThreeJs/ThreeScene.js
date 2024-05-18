5e2575fd2c9c75a924c8d37301eee8ef28acf2a4
SagarKrish1305 <sagarkrish1305@gmail.com>
2024-05-19 04:20:55 +0530

    Done

--- src/Components/ThreeJs/ThreeScene.js
+++ src/Components/ThreeJs/ThreeScene.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import * as THREE from 'three';
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import { FontLoader } from './FontLoader';
+import { TextGeometry } from './TextGeometry';
 
 const ThreeScene = () => {
     useEffect(() => {
